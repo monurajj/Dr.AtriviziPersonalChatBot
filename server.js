f75d1bdb6fd408151afa311bf9525c99427dd5f8
Monu Rajj <monu2feb2004@gmail.com>
2025-02-19 17:16:23 +0530

    testing

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors");
-
+const cors = require("cors"); 
 const app = express();
 app.use(express.json());
 
